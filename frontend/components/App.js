51259eff8b28442df5372541bce4a7750dba39b8
wprince0288 <wprince0288@gmail.com>
2024-10-06 17:37:01 -0500

    completed click handler

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -22,7 +22,7 @@ export default class App extends React.Component {
   postNewTodo = () => {
     axios.post(URL, { name: this.state.todoNameInput })
       .then(res => {
-        this.fetchAllTodos()
+        this.setState({ ...this.state, todos: this.state.todos.concat(res.data.data) })
         this.resetForm()
       })
       .catch(this.setAxiosResponseError)
@@ -40,6 +40,18 @@ export default class App extends React.Component {
       })
       .catch(this.setAxiosResponseError)
   }
+  toggleCompleted = id => () => {
+    axios.patch(`${URL}/${id}`)
+      .then(res => {
+        this.setState({
+          ...this.state, todos: this.state.todos.map(td => {
+            if (td.id !== id) return td
+            return res.data.data
+          })
+        })
+      })
+      .catch(this.setAxiosResponseError)
+  }
 
   componentDidMount() {
     this.fetchAllTodos()
@@ -53,7 +65,7 @@ export default class App extends React.Component {
           <h2>Todos:</h2>
           {
             this.state.todos.map(td => {
-              return <div key={td.id}>{td.name}</div>
+              return <div onClick={this.toggleCompleted(td.id)} key={td.id}>{td.name} {td.completed ? ' ✔️' : ''}</div>
             })
           }
         </div>
