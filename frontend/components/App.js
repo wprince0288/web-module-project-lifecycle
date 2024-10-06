0186a5ec9e20a0dd60df9ec4fae18d4c8a1a8b35
wprince0288 <wprince0288@gmail.com>
2024-10-06 16:03:46 -0500

    todos linked with server

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,43 @@
 import React from 'react'
+import axios from 'axios'
 
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
+  state = {
+    todos: [],
+  }
+  fetchAllTodos = () => {
+    axios.get(URL)
+      .then(res => {
+        this.setState({ ...this.state, todos: res.data.data })
+      })
+      .catch(err => {
+        debugger
+      })
+  }
+  componentDidMount() {
+    this.fetchAllTodos()
+  }
+
   render() {
-    return null
+    return (
+      <div>
+        <div id="error">Error: No error here</div>
+        <div id="todos">
+          <h2>Todos:</h2>
+          {
+           this.state.todos.map(td => {
+            return <div key={td.id}>{td.name}</div>
+           })
+          }
+        </div>
+        <form id="todoForm">
+          <input type="text" placeholder="Type todo"></input>
+          <input type="submit"></input>
+          <button>Clear Completed</button>
+        </form>
+      </div>
+    )
   }
 }
