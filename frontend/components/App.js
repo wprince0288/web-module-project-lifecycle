36c23098810e3a5b388ee3060a0bb51b7a638da7
wprince0288 <wprince0288@gmail.com>
2024-10-06 20:10:40 -0500

    Form component

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import axios from 'axios'
+import Form from './Form'
 
 const URL = 'http://localhost:9000/api/todos'
 
@@ -77,11 +78,13 @@ export default class App extends React.Component {
             }, [])
           }
         </div>
-        <form id="todoForm" onSubmit={this.onTodoFormSubmit}>
-          <input value={this.state.todoNameInput} onChange={this.onTodoNameInputChange} type="text" placeholder="Type todo"></input>
-          <input type="submit"></input>
-          <button onClick={this.toggleDisplayCompleteds}>{this.state.displayCompleteds ? 'Hide' : 'Show'} Completed</button>
-        </form>
+        <Form
+          onTodoFormSubmit={this.onTodoFormSubmit}
+          onTodoNameInputChange={this.onTodoNameInputChange}
+          toggleDisplayCompleteds={this.toggleDisplayCompleteds}
+          todoNameInput={this.state.todoNameInput}
+          displayCompleteds={this.state.displayCompleteds}
+        />
       </div>
     )
   }
--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -2,6 +2,23 @@ import React from 'react'
 
 export default class Form extends React.Component {
   render() {
-    return null
+    return (
+      <>
+        <form id="todoForm" onSubmit={this.props.onTodoFormSubmit}>
+          <input
+            value={this.props.todoNameInput}
+            onChange={this.props.onTodoNameInputChange}
+            type="text"
+            placeholder="Type todo">
+          </input>
+          <input type="submit"></input>
+        </form>
+        <button
+          onClick={this.props.toggleDisplayCompleteds}
+        >
+          {this.props.displayCompleteds ? 'Hide' : 'Show'} Completed
+        </button>
+      </>
+    )
   }
 }
